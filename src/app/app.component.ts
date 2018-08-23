acdec5689c6d159b0a6b68aabaf0f6740c299a53
necraidan <benjamin.auzanneau.tm@gmail.com>
2018-08-23 17:18:24 +0200

    Very first step  🍅🍅🍅

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'funkoPoop';
+export class AppComponent implements OnInit {
+    title = 'funkoPoop';
+
+    funkoList: Funko[];
+
+    constructor(private _http: HttpClient) {}
+
+    ngOnInit(): void {
+        this._http.get('assets/funko.json').subscribe((res: Funko[]) => {
+            this.funkoList = res;
+            console.log(res);
+        });
+    }
+}
+
+interface Funko {
+    name: string;
+    category: string;
+    collection: string;
+    number: number;
+    picture: string[];
+    description: string;
+    magictag: string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MAT_LABEL_GLOBAL_OPTIONS,
+    MatInputModule,
+    MatCardModule
+} from '@angular/material';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, MainNavComponent],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        MatCheckboxModule,
+        LayoutModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatIconModule,
+        MatListModule,
+        MatInputModule,
+        MatCardModule
+    ],
+    providers: [{ provide: MAT_LABEL_GLOBAL_OPTIONS, useValue: { float: 'always' } }],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}

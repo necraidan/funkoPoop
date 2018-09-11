4123a125e5a21aa639156cbdb71720b4170b306f
BuildTools <benjamin.auzanneau.tm@gmail.com>
2018-09-11 22:23:21 +0200

    Add sort by category then name.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,9 +18,20 @@ export class AppComponent implements OnInit {
     ngOnInit(): void {
         this._http.get('assets/funko.json').subscribe((res: Funko[]) => {
             console.log('next !');
-            this.funkoList$.next(res);
+            // tslint:disable:curly
+            const resTri = res.sort((a, b) => {
+                if (a.category > b.category) return 1;
+                if (a.category < b.category) return -1;
+
+                if (a.name < b.name) return -1;
+                if (a.name > b.name) return 1;
+
+                return 0;
+            });
+            // tslint:enable:curly
+            this.funkoList$.next(resTri);
             this.funkoList$.complete();
-            this.funkoFilter = Array.from(res);
+            this.funkoFilter = Array.from(resTri);
         });
     }
 

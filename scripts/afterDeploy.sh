0c9f6ca4048e2bb7b59aaf08cf0523ab9dfcfdae
necraidan <benjamin.auzanneau.tm@gmail.com>
2018-09-25 14:26:21 +0200

    Add afterDeploy script

---
+++ scripts/afterDeploy.sh
@@ -0,0 +1,6 @@
+echo "# FunkoPoop build [![Build Status](https://travis-ci.org/necraidan/funkoPoop.svg?branch=dev)](https://travis-ci.org/necraidan/funkoPoop)
+---------
+Built from dev branch.
+" >> dist/README.md
+
+cp dist/index.html dist/404.html

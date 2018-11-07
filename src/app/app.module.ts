2bc208b73b6116762119baebc5aa01aae6b98474
BuildTools <benjamin.auzanneau.tm@gmail.com>
2018-11-07 23:49:15 +0100

    New rules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,61 +1,57 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { LayoutModule } from '@angular/cdk/layout';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import {
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDialogModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MAT_LABEL_GLOBAL_OPTIONS
+} from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { FunkoDetailComponent } from './funko-list/funko-detail/funko-detail.component';
+import { FunkoListComponent } from './funko-list/funko-list.component';
+import { FunkoRoutingModule } from './funko-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { FilterFunkoPipe } from './shared/pipe/filter-funko-pipe.component';
+
+@NgModule({
+  declarations: [AppComponent, FunkoDetailComponent, FilterFunkoPipe, HeaderComponent, FunkoListComponent],
+  imports: [
+    FunkoRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
     MatCheckboxModule,
+    LayoutModule,
     MatToolbarModule,
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
     MatListModule,
-    MAT_LABEL_GLOBAL_OPTIONS,
     MatInputModule,
     MatCardModule,
     MatDialogModule,
+    ReactiveFormsModule,
     MatRadioModule,
     MatSelectModule,
     MatButtonToggleModule,
     MatChipsModule
-} from '@angular/material';
-
-import { LayoutModule } from '@angular/cdk/layout';
-import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FilterFunkoPipe } from './shared/pipe/filter-funko-pipe.component';
-import { HeaderComponent } from './header/header.component';
-import { FunkoListComponent } from './funko-list/funko-list.component';
-import { FunkoDetailComponent } from './funko-list/funko-detail/funko-detail.component';
-import { RouterModule } from '@angular/router';
-import { FunkoRoutingModule } from './funko-routing.module';
-
-@NgModule({
-    declarations: [AppComponent, FunkoDetailComponent, FilterFunkoPipe, HeaderComponent, FunkoListComponent],
-    imports: [
-        FunkoRoutingModule,
-        BrowserModule,
-        BrowserAnimationsModule,
-        HttpClientModule,
-        MatCheckboxModule,
-        LayoutModule,
-        MatToolbarModule,
-        MatButtonModule,
-        MatSidenavModule,
-        MatIconModule,
-        MatListModule,
-        MatInputModule,
-        MatCardModule,
-        MatDialogModule,
-        ReactiveFormsModule,
-        MatRadioModule,
-        MatSelectModule,
-        MatButtonToggleModule,
-        MatChipsModule
-    ],
-    entryComponents: [FunkoDetailComponent],
-    providers: [{ provide: MAT_LABEL_GLOBAL_OPTIONS, useValue: { float: 'always' } }],
-    bootstrap: [AppComponent]
+  ],
+  entryComponents: [FunkoDetailComponent],
+  providers: [{ provide: MAT_LABEL_GLOBAL_OPTIONS, useValue: { float: 'always' } }],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,113 +1,118 @@
-import { Component, EventEmitter, Input, OnInit, Output, ViewChild, Renderer2 } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, Renderer2, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { AsyncSubject } from 'rxjs';
-import { Funko } from '../app.component';
 import { MatToolbar } from '@angular/material';
 import { ActivatedRoute, Params } from '@angular/router';
+import { AsyncSubject } from 'rxjs';
+import { Funko } from '../app.component';
 
 @Component({
-    // tslint:disable-next-line:component-selector
-    selector: 'header',
-    templateUrl: './header.component.html',
-    styleUrls: ['./header.component.scss']
+  // tslint:disable-next-line:component-selector
+  selector: 'header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-    @ViewChild('extendForm')
-    extendForm: MatToolbar;
+  @ViewChild('extendForm')
+  extendForm: MatToolbar;
 
-    owned: number;
-    categories: string[];
-    collections: string[];
+  owned: number;
+  categories: string[];
+  collections: string[];
 
-    @Input()
-    funkoList$: AsyncSubject<Funko[]>;
+  @Input()
+  funkoList$: AsyncSubject<Funko[]>;
 
-    @Output()
-    change: EventEmitter<Funko[]> = new EventEmitter<Funko[]>();
+  @Output()
+  change: EventEmitter<Funko[]> = new EventEmitter<Funko[]>();
 
-    searchForm = new FormGroup({
-        query: new FormControl(''),
-        radioModel: new FormControl('all'),
-        categorieModel: new FormControl('All'),
-        collectionModel: new FormControl('All')
+  searchForm = new FormGroup({
+    query: new FormControl(''),
+    radioModel: new FormControl('all'),
+    categorieModel: new FormControl('All'),
+    collectionModel: new FormControl('All')
+  });
+  constructor(private _renderer: Renderer2, private _activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.funkoList$.subscribe(fList => {
+      this.owned = fList.filter(elt => elt.owned).length;
+      this.categories = Object.keys(
+        fList.reduce((cats, f) => {
+          cats[f.category] = true;
+          return cats;
+        }, {})
+      ).sort();
+
+      this.collections = Object.keys(
+        fList.reduce((coll, f) => {
+          coll[f.collection] = true;
+          return coll;
+        }, {})
+      ).sort();
+
+      this.categories.unshift('All');
+      this.collections.unshift('All');
     });
-    constructor(private _renderer: Renderer2, private _activatedRoute: ActivatedRoute) {}
 
-    ngOnInit() {
-        this.funkoList$.subscribe(fList => {
-            this.owned = fList.filter(elt => elt.owned).length
-            ;
-            this.categories = Object.keys(
-                fList.reduce((cats, f) => {
-                    cats[f.category] = true;
-                    return cats;
-                }, {})
-            ).sort();
+    this.searchForm.valueChanges.subscribe(values => {
+      this.pushState(values);
 
-            this.collections = Object.keys(
-                fList.reduce((coll, f) => {
-                    coll[f.collection] = true;
-                    return coll;
-                }, {})
-            ).sort();
+      setTimeout(() => {
+        this.filtering();
+      }, 0);
+    });
 
-            this.categories.unshift('All');
-            this.collections.unshift('All');
-        });
+    this._activatedRoute.queryParams.subscribe((params: Params) => {
+      // tslint:disable-next-line:no-unused-expression
+      params.query && this.searchForm.patchValue(params);
+    });
+  }
 
-        this.searchForm.valueChanges.subscribe(values => {
-            this._pushState(values);
+  resetForm() {
+    this.searchForm.patchValue({ radioModel: 'all', categorieModel: 'All', collectionModel: 'All' });
+  }
 
-            setTimeout(() => {
-                this._filtering();
-            }, 0);
-        });
-
-        this._activatedRoute.queryParams.subscribe((params: Params) => {
-            // tslint:disable-next-line:no-unused-expression
-            params.query && this.searchForm.patchValue(params);
-        });
+  toggleExtendForm() {
+    if (this.extendForm._elementRef.nativeElement.classList.contains('open')) {
+      this._renderer.removeClass(this.extendForm._elementRef.nativeElement, 'open');
+    } else {
+      this._renderer.addClass(this.extendForm._elementRef.nativeElement, 'open');
     }
+  }
 
-    resetForm() {
-        this.searchForm.patchValue({ radioModel: 'all', categorieModel: 'All', collectionModel: 'All' });
-    }
-
-    toggleExtendForm() {
-        if (this.extendForm._elementRef.nativeElement.classList.contains('open')) {
-            this._renderer.removeClass(this.extendForm._elementRef.nativeElement, 'open');
-        } else {
-            this._renderer.addClass(this.extendForm._elementRef.nativeElement, 'open');
-        }
-    }
-
-    private _filtering() {
-        this.funkoList$.subscribe(fList => {
-            this.change.emit(
-                fList
-                    .filter(f => {
-                        return this.searchForm.get('radioModel').value === 'all' ? true : f[this.searchForm.get('radioModel').value];
-                    })
-                    .filter(f => {
-                        return this.searchForm.get('categorieModel').value === 'All' ? true : f.category === this.searchForm.get('categorieModel').value;
-                    })
-                    .filter(f => {
-                        return this.searchForm.get('collectionModel').value === 'All' ? true : f.collection === this.searchForm.get('collectionModel').value;
-                    })
-                    .filter(funko => {
-                        const val = this.searchForm.get('query').value;
-                        return (
-                            funko.name.toLowerCase().includes(val.toLowerCase()) ||
-                            funko.category.toLowerCase().includes(val.toLowerCase()) ||
-                            funko.collection.toLowerCase().includes(val.toLowerCase()) ||
-                            funko.number.toLowerCase().includes(val.toLowerCase())
-                        );
-                    })
+  private filtering() {
+    this.funkoList$.subscribe(fList => {
+      this.change.emit(
+        fList
+          .filter(f => {
+            return this.searchForm.get('radioModel').value === 'all' ? true : f[this.searchForm.get('radioModel').value];
+          })
+          .filter(f => {
+            return this.searchForm.get('categorieModel').value === 'All'
+              ? true
+              : f.category === this.searchForm.get('categorieModel').value;
+          })
+          .filter(f => {
+            return this.searchForm.get('collectionModel').value === 'All'
+              ? true
+              : f.collection === this.searchForm.get('collectionModel').value;
+          })
+          .filter(funko => {
+            const val = this.searchForm.get('query').value;
+            return (
+              funko.name.toLowerCase().includes(val.toLowerCase()) ||
+              funko.category.toLowerCase().includes(val.toLowerCase()) ||
+              funko.collection.toLowerCase().includes(val.toLowerCase()) ||
+              funko.number.toLowerCase().includes(val.toLowerCase())
             );
-        });
-    }
+          })
+      );
+    });
+  }
 
-    private _pushState(arg) {
-        return arg.query ? window.history.pushState(undefined, 'query', 'search?query=' + arg.query) : window.history.pushState(undefined, 'query', '');
-    }
+  private pushState(arg) {
+    return arg.query
+      ? window.history.pushState(undefined, 'query', 'search?query=' + arg.query)
+      : window.history.pushState(undefined, 'query', '');
+  }
 }

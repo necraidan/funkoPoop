e98c6b6deee55ca454d2def1c8c89d33c91858a1
Benjamin Auzanneau <benjamin.auzanneau.tm@gmail.com>
2018-11-15 11:11:33 +0100

    Update funko-list.component.ts

--- src/app/funko-list/funko-list.component.ts
+++ src/app/funko-list/funko-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatSort, MatTableDataSource, MatDialog } from '@angular/material';
 import { FunkoFilterService } from '../shared/service/funko-filter.service';
 import { Funko } from '../app.component';
+import { FunkoDetailComponent } from './funko-detail/funko-detail.component';
 
 @Component({
   // tslint:disable-next-line:component-selector

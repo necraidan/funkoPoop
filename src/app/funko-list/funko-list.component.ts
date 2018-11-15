683b6c7ba37ff372a20084fe76c8b3b25f2064fc
Benjamin Auzanneau <benjamin.auzanneau.tm@gmail.com>
2018-11-15 11:05:55 +0100

    Add dialog on click

--- src/app/funko-list/funko-list.component.ts
+++ src/app/funko-list/funko-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatSort, MatTableDataSource } from '@angular/material';
+import { MatSort, MatTableDataSource, MatDialog } from '@angular/material';
 import { FunkoFilterService } from '../shared/service/funko-filter.service';
 
 @Component({
@@ -15,12 +15,23 @@ export class FunkoListComponent implements OnInit {
   @ViewChild(MatSort)
   sort: MatSort;
 
-  constructor(private funkoFilterService: FunkoFilterService) {}
+  constructor(private dialog: MatDialog, private funkoFilterService: FunkoFilterService) {}
 
   ngOnInit() {
     this.funkoFilterService.funkoFiler$.subscribe(list => {
       this.dataSource = new MatTableDataSource(list);
       this.dataSource.sort = this.sort;
+    });
+  }
+  
+  openDetail(funko: Funko) {
+    const dialogRef = this.dialog.open(FunkoDetailComponent, {
+      width: '500px',
+      data: funko
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+      console.log('The dialog was closed');
     });
   }
 }

c875f3cd723ed699e25be42a6eaacfb7f848b5c7
BuildTools <benjamin.auzanneau.tm@gmail.com>
2019-01-05 15:24:17 +0100

    Add some new funko

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,6 +20,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.http.get('assets/funko.json').subscribe((res: Funko[]) => {
+      console.log(res);
       // tslint:disable:curly
       const resTri = res.sort((a, b) => {
         if (a.category > b.category) return 1;

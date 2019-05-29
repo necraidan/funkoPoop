70b8b7732e24641b65523342fed122c6883636bc
necraidan <benjamin.auzanneau.tm@gmail.com>
2019-05-29 11:57:39 +0200

    Update config and add web worker example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,7 +22,6 @@ export class AppComponent implements OnInit {
     });
 
     if (this.swUpdate.isEnabled) {
-
       this.swUpdate.available.subscribe(() => {
         if (confirm('New version available. Load New Version?')) {
           window.location.reload();
